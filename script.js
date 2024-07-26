3321d5945a34b1fd3e837ecaf826ff871990d540
omcdevs <crand212@umn.edu>
2024-07-25 22:32:19 -0500

    Home Page Update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Computer Science Student"],
+        strings: ["Computer Science Student", "Software Engineer","Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Computer Science Student"],
+        strings: ["Computer Science Student", "Software Engineer","Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
